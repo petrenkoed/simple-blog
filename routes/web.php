390907a17096f9923a6d77355a88976c3a25b51a
petrenkoed <petrena2018@mail.ru>
2022-02-15 18:56:15 +0500

    bootstrap, auth

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('test');
+    return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
