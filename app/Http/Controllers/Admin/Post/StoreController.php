78b1ae7120cfda5ad888420b89e561664ba7c5c8
petrenkoed <petrena2018@mail.ru>
2022-02-23 17:12:37 +0500

    update Post

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -11,29 +11,18 @@ class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
-//        try {
-//            $data = $request->validated();
-//            $tagIds = $data['tag_ids'];
-//            unset($data['tag_ids']);
-//
-//            $data['preview_image'] = Storage::put('/images', $data['preview_image']);
-//            $data['main_image'] = Storage::put('/images', $data['main_image']);
-//            $post = Post::firstOrCreate($data);
-//            $post->tags()->attach($tagIds);
-//        } catch (\Exception) {
-//            abort(404);
-//        }
+        try {
+            $data = $request->validated();
 
-
-        $data = $request->validated();
-
-        $tagIds = $data['tag_ids'];
-        unset($data['tag_ids']);
-        $data['preview_image'] = Storage::put('/images', $data['preview_image']);
-        $data['main_image'] = Storage::put('/images', $data['main_image']);
-        $post = Post::firstOrCreate($data);
-        $post->tags()->attach($tagIds);
-
+            $tagIds = $data['tag_ids'];
+            unset($data['tag_ids']);
+            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
+            $post = Post::firstOrCreate($data);
+            $post->tags()->attach($tagIds);
+        } catch (\Exception) {
+            abort(404);
+        }
         return redirect()->route('admin.post.index');
 
     }
--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -26,22 +26,98 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
 
-                <div class="row">
+                <div class="row d-flex justify-content-center">
                     <div class="col-12 h4">
                         Редактирование поста
                     </div>
                     <div class="col-6">
-                        <form action="{{ route('admin.post.update', $post->id ) }}" method="POST">
+                        <form action="{{ route('admin.post.update', $post->id ) }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             @method('PATCH')
                             <div class="form-group">
                                 <label for="title-post">Название поста</label>
-                                <input type="text" class="form-control" id="title-post" name="title" placeholder="Введите название" value="{{ $post->title }}">
+                                <input type="text"
+                                       class="form-control"
+                                       id="title-post"
+                                       name="title"
+                                       placeholder="Введите название"
+                                       value="{{ $post->title }}"
+                                >
                                 @error('title')
-                                    <div class="text-danger">Это поле необходимо заполнить. <br> {{ $message }}</div>
+                                <div class="text-danger">Это поле необходимо заполнить. <br> {{ $message }}</div>
                                 @enderror
                             </div>
-                            <input type="submit" class="btn btn-primary mt-3" value="Редактировать">
+                            <div class="form-group">
+                                <textarea id="summernote"
+                                          name="content"
+                                >{{ $post->content }}</textarea>
+                                @error('content')
+                                <div class="text-danger">Это поле необходимо заполнить. <br> {{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputFile">Добавить превью</label>
+                                <div class="">
+                                    <img class="w-25" src="{{ asset('storage/'.$post->preview_image) }}" alt="preview_image">
+                                </div>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="preview_image">
+                                        <label class="custom-file-label" >Выберите изображение</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Загрузка</span>
+                                    </div>
+                                </div>
+                                @error('preview_image')
+                                <div class="text-danger">Это поле необходимо заполнить. <br> {{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputFile">Добавить главное изображение</label>
+                                <div class="w-25">
+                                    <img src="{{ asset('storage/'.$post->main_image) }}" alt="main_image">
+                                </div>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="main_image">
+                                        <label class="custom-file-label" >Выберите изображение</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Загрузка</span>
+                                    </div>
+                                </div>
+                                @error('main_image')
+                                <div class="text-danger">Это поле необходимо заполнить. <br> {{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label>Выберите категорию</label>
+                                <select class="form-control" name="category_id">
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id  }}"
+                                            {{ $category->id == $post->category_id ? ' selected' : '' }}
+                                        >{{ $category->title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label>Теги</label>
+                                <select class="select2" name="tag_ids[]" multiple="multiple" data-placeholder="Выберите теги" style="width: 100%;">
+                                    @foreach($tags as $tag)
+                                        <option
+                                            {{ is_array($post->tags->pluck('id')->toArray()) && in_array($tag->id, $post->tags->pluck('id')->toArray()) ? ' selected' : '' }}
+                                            value="{{ $tag->id }}">{{ $tag->title }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary mt-3 w-50" value="Редактировать">
+                            </div>
                         </form>
                     </div>
                 </div>
