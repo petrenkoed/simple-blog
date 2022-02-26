74d30c97bc8087d3279c44c8e6a5ec4bacdd453a
petrenkoed <petrena2018@mail.ru>
2022-02-26 17:09:37 +0500

    Понятие очереди, Notification класс

--- app/Mail/User/Password.php
+++ app/Mail/User/Password.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class Password extends Mailable
+class Password extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
