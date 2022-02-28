fae57d4983c12a86e1db6711857fc64d7d36b108
petrenkoed <petrena2018@mail.ru>
2022-02-28 18:45:03 +0500

    Выводим посты с пагинацией. А также случайные посты и популярные, сортируя их по количеству лайков

--- app/Http/Controllers/Personal/Main/IndexController.php
+++ app/Http/Controllers/Personal/Main/IndexController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Personal\Main;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Post;
+use App\Models\PostUserLike;
 use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -13,6 +15,9 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('personal.main.index');
+        $data = [];
+
+//        $data['CountComment'] = Comment::all()->count();
+        return view('personal.main.index', compact('data'));
     }
 }
